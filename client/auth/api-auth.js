585af8ea66d4b7e4a2d315713c3189c94d4816a1
WebMobik <ddimon3000@bk.ru>
2020-11-17 11:47:45 +0300

    functional authenticate user

---
+++ client/auth/api-auth.js
@@ -0,0 +1,29 @@
+const { response } = require("express")
+
+const signin = async (user) => {
+    try {
+        const response = await fetch('/api/signin/', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify(user)
+        })
+        return await response.json()
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+const signout = async () => {
+    try {
+        const response = await fetch('/api/signout/', { method: 'GET' })
+        return await response.json()
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export {signin, signout}
